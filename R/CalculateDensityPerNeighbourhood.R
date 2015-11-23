da7aa7dd1087e3e444c0ad6f693c8ec19287b194
BarbaraSienkiewicz <barbarasienkiewicz@hotmail.com>
2015-11-23 14:47:28 +0100

    density script

--- R/CalculateDensityPerNeighbourhood.R
+++ R/CalculateDensityPerNeighbourhood.R
@@ -1,7 +1,6 @@
 library(sp)
 library(rgdal)
 library(shapefiles)
-library(maptools)
 
 
 Density <- function(SHPfilename) {
@@ -24,23 +23,24 @@ Density <- function(SHPfilename) {
   ## Create matrix containing all aggregation level ID's
   ID <- neigh$ID
   ID <- as.matrix(ID)
-  colnames(ID)[1]<-"ID"
   
-  ## Create empty data frame and fil it with ID column
-  df <- data.frame()
-  df <- ID
+  ## Create data frame (neighTab) containing area varibles (total area, water area, etc.)
+  neighTab <- data.frame()
+  neighTab <- ID
+  colnames(neighTab)[1] <- "ID"
+  neighTab <- merge(neighTab,neigh,by.x= "ID", by.y = "ID", all = TRUE)
   
   ## Load accidents
   dsn3 <- file.path("Output", "accidentsScenatio1.shp")
-  ogrListLayers(dsn3) ## to find out what the layers are
+  #ogrListLayers(dsn3) ## to find out what the layers are
   acci <- readShapePoints(dsn3)
-  proj4string(acci) <- prj_string_RD 
+  proj4string(acci) <- prj_string_RD
   
   ## Check CRS
   proj4string(neigh) 
   proj4string(acci)
-  #identicalCRS(accidentsSpa,neigh)
   
+  ## Calculate accident density per aggregation level
   aggre <- over(acci, neigh)
   count <- table(aggre$ID, exclude = NULL)
   count <- data.frame(count)
@@ -48,18 +48,19 @@ Density <- function(SHPfilename) {
   neighID <- count$Var1
   accidentsTab <- cbind(frequency, neighID)
   accidentsTab <- as.matrix(accidentsTab)
-  df <- merge(ID,accidentsTab,by.x= "ID", by.y = "neighID", all = TRUE)
-  colnames(df)[2] <- "accidents"
   
-  colnames(df)
+  ## Mere accident density (accidentTab) with neighTab
+  df <- merge(neighTab,accidentsTab,by.x= "ID", by.y = "neighID", all = TRUE)
+  colnames(df)[7] <- "accidents"
+  
   print(df)
   
   ## Create a list of factor files
   FactorList <- list.files("Data/Factors", pattern = '.shp')
-  print(FactorList)
   
   
   for (j in 1:length(FactorList)) {    
+    
     ## Select [j] factor
     filename <- FactorList[j]
     
@@ -68,14 +69,6 @@ Density <- function(SHPfilename) {
     ogrListLayers(dsn2)
     factor <- readShapePoints(dsn2)
     proj4string(factor) <- prj_string_RD 
-    
-    # Check classes
-    class(neigh)
-    class(factorsSpat)
-    
-    ## Check CRS
-    proj4string(neigh) 
-    proj4string(factor)
     
     ## Count point in polygons
     aggre <- over(factor, neigh)
@@ -87,19 +80,17 @@ Density <- function(SHPfilename) {
     FreTab <- cbind(neighID2, frequency2)
     FreTab <- as.matrix(FreTab)
     
-    #merged <- merge(df,factorTab,by.x= "ID", by.y = "neighID2", all = TRUE)
-    #factorDens <- merged[3]
-    
+    ## Add frequency of the j factor to df dataframe
     df <- merge(df,FreTab,by.x= "ID", by.y = "neighID2", all = TRUE)
     
+    ## change column names
     factorname <- gsub(".shp", "", filename)
-    colnames(df)[j+2] <- factorname
+    colnames(df)[j+8] <- factorname
   }
   
   OutputDir <- paste("Output/", tablename, ".csv")
   OutputDir <- gsub(" ", "", OutputDir)
   print(OutputDir)
   finaldf <- df[-nrow(df),]
-  finaldf[is.na(finaldf)] <- 0
-  output <- write.csv(finaldf, OutputDir)
+  output <- write.table(finaldf, OutputDir, sep = ';', na = '0', dec = ',', col.names = TRUE, row.names = FALSE)
 }
